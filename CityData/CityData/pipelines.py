549329e0464b00165563af67c74d336ec1e021e5
bkawan <bikeshkawang@gmail.com>
2016-08-07 23:00:14 +0545

    json formatted successfull

--- CityData/CityData/pipelines.py
+++ CityData/CityData/pipelines.py
@@ -4,50 +4,49 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from collections import  OrderedDict
 import json
 import codecs
 json_path  = "data/"
 
 class CitydataPipeline(object):
     def process_item(self, item, spider):
-        self.file = codecs.open('{}{}.json'.format(json_path,item['stats']['zip']), 'wb',
+        self.file = codecs.open('../../{}.json'.format(item['stats']['zip']), 'wb',
                                 encoding='utf-8')
-        zip_items= {
-            'cities': item['stats']['cities'],
-            'county': item['stats']['county'],
-            'population_25_years_and_above': item['stats']['population_25_years_and_above'],
-            'population_15_years_and_above': item['stats']['population_15_years_and_above'],
-            'real_etates': item['stats']['real_etates'],
-            'housing_units_in_structures': item['stats']['housing_units_in_structures'],
-            'house_values_of_owner_occupied': item['stats']['house_values_of_owner_occupied'],
-            'household_income_distribution': item['stats']['household_income_distribution'],
-            'bedrooms_in_houses_and_apartments': item['stats']['bedrooms_in_houses_and_apartments'],
-            'vehicles_in_houses_and_apartments': item['stats']['vehicles_in_houses_and_apartments'],
-            'mean_house_price': item['stats']['mean_house_price'],
-            'median_house_value': item['stats']['median_house_value'],
-            'entry_of_foreign_born_population': item['stats']['entry_of_foreign_born_population'],
-            'travel_time_to_work': item['stats']['travel_time_to_work'],
-            'races': item['stats']['races'],
-            'rent_paid_by_renters': item['stats']['rent_paid_by_renters'],
-            'first_ancestries': item['stats']['first_ancestries'],
-            'neighborhoods': item['stats']['neighborhoods'],
-            'foreign_born_residents': item['stats']['foreign_born_residents'],
-            'transportation': item['stats']['transportation'],
-            'facilities': item['stats']['facilities']
+
+
+
+        values = OrderedDict([
+        ('zip', item['stats']['zip']),
+        ('cities', item['stats']['cities']),
+        ('county',item['stats']['county']),
+        ('population_25_years_and_above', item['stats']['population_25_years_and_above']),
+        ('population_15_years_and_above', item['stats']['population_15_years_and_above']),
+        ('real_etates', item['stats']['real_etates']),
+        ('housing_units_in_structures', item['stats']['housing_units_in_structures']),
+        ('house_values_of_owner_occupied', item['stats']['house_values_of_owner_occupied']),
+        ('household_income_distribution', item['stats']['household_income_distribution']),
+        ('bedrooms_in_houses_and_apartments', item['stats']['bedrooms_in_houses_and_apartments']),
+        ('vehicles_in_houses_and_apartments', item['stats']['vehicles_in_houses_and_apartments']),
+        ('mean_house_price', item['stats']['mean_house_price']),
+        ('median_house_value', item['stats']['median_house_value']),
+        ('entry_of_foreign_born_population', item['stats']['entry_of_foreign_born_population']),
+        ('travel_time_to_work', item['stats']['travel_time_to_work']),
+        ('races', item['stats']['races']),
+        ('rent_paid_by_renters', item['stats']['rent_paid_by_renters']),
+        ('first_ancestries', item['stats']['first_ancestries']),
+        ('neighborhoods', item['stats']['neighborhoods']),
+        ('foreign_born_residents', item['stats']['foreign_born_residents']),
+        ('transportation', item['stats']['transportation']),
+        ('facilities', item['stats']['facilities'])
+
+        ])
+        item = {
+            'stats' : values
+
         }
 
-        zip = {
-            item['stats']['zip'] : zip_items
-        }
-        stats = {
-            'stats': {
-                'zip': zip,
-
-            }
-        }
-
-        line = json.dumps(dict(stats), ) + "\n"
+        line = json.dumps(item, sort_keys=False ) + "\n"
 
         self.file.write(line)
 
